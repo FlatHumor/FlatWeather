4738f2305a48f12009ca878087e65fd14f4a9e4d
Mark <m.sudnev@gmail.com>
2018-07-26 21:33:35 +0300

    __

--- app/src/main/java/ru/inpleasure/weather/Contract.java
+++ app/src/main/java/ru/inpleasure/weather/Contract.java
@@ -1,5 +1,9 @@
 package ru.inpleasure.weather;
 
+import android.os.Looper;
+import android.location.Location;
+import android.content.Context;
+
 import ru.inpleasure.weather.model.dbo.Weather;
 import ru.inpleasure.weather.api.dto.WeatherDto;
 
@@ -8,6 +12,13 @@ public interface Contract
     interface Api
     {
         WeatherDto getWeather();
+    }
+    
+    interface LocationService
+    {
+        Location getLocation();
+        String getLatitude();
+        String getLongitude();
     }
     
     interface Model
@@ -20,6 +31,9 @@ public interface Contract
     {
         void onRefreshButtonClick();
         void onHistoryButtonClick();
+        void onLocationReceived(Location location);
+        Context getContext();
+        Looper getLooper();
         void onDestroy();
     }
     
