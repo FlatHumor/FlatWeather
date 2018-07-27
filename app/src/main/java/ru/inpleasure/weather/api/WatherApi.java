0f1ed2aa6d29633bf50bdbd45ea9eb83e5d449f6
Mark <m.sudnev@gmail.com>
2018-07-28 00:52:27 +0300

    Update WeatherApi

--- app/src/main/java/ru/inpleasure/weather/api/WatherApi.java
+++ app/src/main/java/ru/inpleasure/weather/api/WatherApi.java
@@ -1,10 +1,14 @@
 package ru.inpleasure.weather.api;
 
 import ru.inpleasure.weather.Contract;
+import ru.inpleasure.weather.api.dto.WeatherDto;
 
 public class WatherApi
     implements Contract.Api
 {
     public static final String BASE_API = "";
     
+    public WeatherDto getWeather() {
+        return null;
+    }
 }
