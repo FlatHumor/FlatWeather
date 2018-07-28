d4ea0795dab2744e559ff518f6fff40f81ccae3f
Mark <i.skotinin@live.com>
2018-07-28 10:11:30 +0300

    Update WatherApi.java

--- app/src/main/java/ru/inpleasure/weather/api/WatherApi.java
+++ app/src/main/java/ru/inpleasure/weather/api/WatherApi.java
@@ -6,7 +6,7 @@ import ru.inpleasure.weather.api.dto.WeatherDto;
 public class WatherApi
     implements Contract.Api
 {
-    public static final String BASE_API = "";
+    public static final String BASE_API = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s";
     
     public WeatherDto getWeather() {
         return null;
