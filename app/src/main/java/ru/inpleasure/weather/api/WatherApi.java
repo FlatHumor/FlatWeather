ab9e1026255cc69a69422d06baadb51ab3be787b
Mark <i.skotinin@live.com>
2018-07-28 10:17:03 +0300

    Update WatherApi.java

--- app/src/main/java/ru/inpleasure/weather/api/WatherApi.java
+++ app/src/main/java/ru/inpleasure/weather/api/WatherApi.java
@@ -8,6 +8,22 @@ public class WatherApi
 {
     public static final String BASE_API = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s";
     
+    private String getResponse(String url)
+    {
+        OkHttpClient httpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try {
+            Response response = httpClient.newCall(request).execute();
+            return response.body().string();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+    
     public WeatherDto getWeather() {
         return null;
     }
